97f9a6f664783f97ffa8f1fdccef9dd039949c64
mckelvin <mckelvin@users.noreply.github.com>
2016-01-13 00:19:03 +0800

    db: Use sqlite

--- cheapflight/config.py
+++ cheapflight/config.py
@@ -1,5 +1,10 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 class DevelopmentConfig(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql://dev:dev@localhost/dev_cheapflight"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MEMCACHED_SERVERS = ["localhost:11211"]
+    PERMDIRS = os.path.join(PROJECT_DIR, "permdirs")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///%s" % os.path.join(PERMDIRS, "database.sqlite")
